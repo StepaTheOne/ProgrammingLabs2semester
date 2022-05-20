aa8d103e66aed7a4a2a6c2a39de2f0c1cef15d70
TheOnlyOne <stepatheone@gmail.com>
2022-05-20 22:26:47 +0700

    Start lab3
    Сделал матрицу, вроде рандомная.

---
+++ Lab3/Matrix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Matrix
+    {
+        Random rnd = new Random();
+        private int n;
+        private int[,] matrix;
+
+        public Matrix(int n)
+        {
+            this.n = n;
+            matrix = new int[this.n,this.n];
+            for(int i = 0; i < this.n; i++)
+            {
+                for(int j = 0; j < this.n; j++)
+                {
+                    matrix[i,j] = rnd.Next(0,100);
+                }
+            }
+        }
+
+
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            Matrix matrix = new Matrix(n);
+            matrix.PrintMatrix();
+        }
+    }
+}
